f5361da39fe9043f7b464656629477231b38f305
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2022-01-11 18:48:59 +0530

    Navbar changes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,310 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Stack } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import EditIcon from "@mui/icons-material/Edit";
+import Divider from "@mui/material/Divider";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Modal from "@mui/material/Modal";
+import Login from "./Login";
+
+const Search = styled("div")(({ theme }) => ({
+  borderRadius: "2px",
+  height: "36px",
+  width: "564px !important",
+  backgroundColor: "white",
+
+  padding: "0 16px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  cursor: "pointer",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "black",
+  width: "80%",
+}));
+
+const LoginButton = styled(Button)(({ theme }) => ({
+  "&:hover": {
+    backgroundColor: "white",
+  },
+}));
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
+
+
+export default function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [openModal, setOpenModal] = React.useState(false);
+
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="fixed"
+        sx={{ boxShadow: "none", height: "56px", justifyContent: "center" }}
+      >
+        <Toolbar sx={{ minHeight: "auto !important" }}>
+          <Stack
+            direction="row"
+            spacing={4}
+            align="center"
+            sx={{
+              maxWidth: "1248px",
+              margin: "0 auto",
+
+              width: "100%",
+            }}
+          >
+            {/* Logo Item */}
+            <Stack direction="column">
+              <img
+                width="75"
+                src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+                alt="Flipkart"
+                title="Flipkart"
+              />
+
+              <div style={{ fontSize: "11px", fontStyle: "italic" }}>
+                Explore
+                <span
+                  style={{
+                    color: "#ffe500",
+                    fontWeight: "500",
+                    margin: "0 2px",
+                  }}
+                >
+                  Plus
+                </span>
+                <img
+                  width="10"
+                  src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png"
+                  alt="logo"
+                ></img>
+              </div>
+            </Stack>
+
+            {/* Search Bar */}
+            <Search>
+              <StyledInputBase
+                placeholder="Search for products, brands and more"
+                inputProps={{ "aria-label": "search" }}
+              />
+              <SearchIconWrapper>
+                <SearchIcon sx={{ color: "blue" }} />
+              </SearchIconWrapper>
+            </Search>
+            <div style={{ width: "150px" }}></div>
+
+            <Stack direction="row" spacing={3}>
+              <div>
+                <LoginButton
+                  id="demo-login-button"
+                  aria-controls={open ? "demo-customized-login" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="contained"
+                  disableElevation
+                  onClick={handleOpenModal}
+                  //   onMouseEnter={handleClick}
+                  //   onMouseLeave={handleClose}
+
+                  sx={{
+                    backgroundColor: "white",
+                    color: "blue",
+                    borderRadius: "2px",
+                    padding: "5px 40px",
+                  }}
+                >
+                  Login
+                </LoginButton>
+
+                <Modal
+                  open={openModal}
+                  onClose={handleCloseModal}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                
+                    <Login />
+               
+                </Modal>
+
+                <StyledMenu
+                  id="demo-customized-login"
+                  MenuListProps={{
+                    "aria-labelledby": "demo-login-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <FileCopyIcon />
+                    Duplicate
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArchiveIcon />
+                    Archive
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <MoreHorizIcon />
+                    More
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+
+              <div>
+                <Button
+                  sx={{
+                    boxShadow: "none",
+                    "&:hover": {
+                      backgroundColor: "transparent !important",
+                      boxShadow: "none",
+                    },
+                  }}
+                  id="demo-customized-button"
+                  aria-controls={open ? "demo-customized-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="contained"
+                  disableElevation
+                  onMouseOver={handleClick}
+                  endIcon={<KeyboardArrowDownIcon />}
+                >
+                  Menu
+                </Button>
+
+                <StyledMenu
+                  id="demo-customized-menu"
+                  MenuListProps={{
+                    "aria-labelledby": "demo-customized-button",
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <EditIcon />
+                    Edit
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <FileCopyIcon />
+                    Duplicate
+                  </MenuItem>
+                  <Divider sx={{ my: 0.5 }} />
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <ArchiveIcon />
+                    Archive
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} disableRipple>
+                    <MoreHorizIcon />
+                    More
+                  </MenuItem>
+                </StyledMenu>
+              </div>
+
+              <Button
+                sx={{
+                  boxShadow: "none",
+                  "&:hover": {
+                    backgroundColor: "transparent !important",
+                    boxShadow: "none",
+                  },
+                }}
+                variant="contained"
+                startIcon={<ShoppingCartIcon />}
+              >
+                Cart
+              </Button>
+            </Stack>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
